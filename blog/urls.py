1db743937429641d997592eb3a3f90ef83abb495
Hwieun <gk1vk@naver.com>
2019-08-30 02:04:44 +0900

    templete extending

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
 #root url에 post_list라는 view 할당 #name에 넣은 것은 url에 이름을 붙인 것으로, 뷰를 식별
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
